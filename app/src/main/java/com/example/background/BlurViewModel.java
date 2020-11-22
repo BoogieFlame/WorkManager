670ca84dbc8f879abaae9ebb5df77594e45bf015
BoogieFlame <kastor.vip@gmail.com>
2020-11-23 00:46:47 +0700

    Добавление ввода и вывода фотографий

--- app/src/main/java/com/example/background/BlurViewModel.java
+++ app/src/main/java/com/example/background/BlurViewModel.java
@@ -18,16 +18,26 @@ package com.example.background;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.work.Data;
 import androidx.work.OneTimeWorkRequest;
 import androidx.work.WorkManager;
 import android.app.Application;
 import android.net.Uri;
 import android.text.TextUtils;
 import com.example.background.workers.BlurWorker;
+import static com.example.background.Constants.KEY_IMAGE_URI;
 
 public class BlurViewModel extends AndroidViewModel {
 
     private Uri mImageUri;
+
+    private Data createInputDataForUri() {
+        Data.Builder builder = new Data.Builder();
+        if (mImageUri != null) {
+            builder.putString(KEY_IMAGE_URI, mImageUri.toString());
+        }
+        return  builder.build();
+    }
 
     private WorkManager mWorkManager;
     // BlurViewModel constructor
@@ -38,8 +48,11 @@ public class BlurViewModel extends AndroidViewModel {
     }
 
     void applyBlur(int blurLevel) {
-        mWorkManager.enqueue(OneTimeWorkRequest.from(BlurWorker.class));
-
+        OneTimeWorkRequest blueRequest =
+                new OneTimeWorkRequest.Builder(BlurWorker.class).setInputData(createInputDataForUri())
+                        .build();
+        mWorkManager.enqueue(blueRequest);
+        //mWorkManager.enqueue(OneTimeWorkRequest.from(BlurWorker.class));
     }
 
     private Uri uriOrNull(String uriString) {
--- app/src/main/java/com/example/background/workers/BlurWorker.java
+++ app/src/main/java/com/example/background/workers/BlurWorker.java
@@ -1,14 +1,19 @@
 package com.example.background.workers;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.text.TextUtils;
 import android.util.Log;
 import com.example.background.R;
 import androidx.annotation.NonNull;
+import androidx.work.Data;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
+
+import static com.example.background.Constants.KEY_IMAGE_URI;
 
 public class BlurWorker extends Worker {
     public BlurWorker(
@@ -21,10 +26,20 @@ public class BlurWorker extends Worker {
     @Override
     public Result doWork() {
         Context applicationContext = getApplicationContext();
+        String resourceUri = getInputData().getString(KEY_IMAGE_URI);
         try {
-            Bitmap picture = BitmapFactory.decodeResource(
-                    applicationContext.getResources(),
-                    R.drawable.test);
+//            Bitmap picture = BitmapFactory.decodeResource(
+//                    applicationContext.getResources(),
+//                    R.drawable.test);
+            if (TextUtils.isEmpty(resourceUri)) {
+                Log.e(TAG, "Invalid input uri");
+                throw new IllegalArgumentException("Invalid input uri");
+            }
+            ContentResolver resolver = applicationContext.getContentResolver();
+            // Create a bitmap
+            Bitmap picture = BitmapFactory.decodeStream(
+                    resolver.openInputStream(Uri.parse(resourceUri)));
+
             // Blur the bitmap
             Bitmap output = WorkerUtils.blurBitmap(picture, applicationContext);
 
@@ -34,7 +49,11 @@ public class BlurWorker extends Worker {
                     + outputUri.toString(), applicationContext);
 
             // If there were no errors, return SUCCESS
-            return Result.success();
+//            return Result.success();
+            Data outputData = new Data.Builder()
+                    .putString(KEY_IMAGE_URI, outputUri.toString())
+                    .build();
+            return Result.success(outputData);
         }
         catch (Throwable throwable) {
             // Technically WorkManager will return Result.failure()
